5dd2cda149d0131a474a3dea694c0a9ea6add1b6
ManL512 <miguelnavas933@gmail.com>
2025-09-23 16:52:12 -0500

    ejercicio 9

---
+++ app/src/main/java/com/losmoviles/oopbasicexercises/Ejercicio9.kt
@@ -0,0 +1,33 @@
+package com.losmoviles.oopbasicexercises
+
+fun main() {
+
+}
+
+class Estudiante{
+    val nombre: String = ""
+    val id: Int = 0
+    var calificaciones: MutableList<Double> = TODO()
+
+    fun promedioEstudiantes(){
+
+    }
+}
+
+class Curso {
+    val nombreCurso: String = ""
+    var listadoEstudiantes: MutableList<Estudiante> = TODO()
+
+    fun agregarEstudiante(){
+
+    }
+
+    fun eliminarEstudiante(){
+
+    }
+
+    fun mejorEstudiante(){
+
+    }
+}
+
