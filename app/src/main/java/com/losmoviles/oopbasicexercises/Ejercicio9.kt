62991a59c457da1589e1cbc4bf7c2e32ed4f693d
ManL512 <miguelnavas933@gmail.com>
2025-09-24 08:17:09 -0500

    ejercicio 9 - logica didier

--- app/src/main/java/com/losmoviles/oopbasicexercises/Ejercicio9.kt
+++ app/src/main/java/com/losmoviles/oopbasicexercises/Ejercicio9.kt
@@ -3,31 +3,48 @@ package com.losmoviles.oopbasicexercises
 fun main() {
 
 }
-
-class Estudiante{
+class Estudiante {
     val nombre: String = ""
     val id: Int = 0
-    var calificaciones: MutableList<Double> = TODO()
+    var calificaciones: MutableList<Double> = mutableListOf()
 
-    fun promedioEstudiantes(){
-
+    fun promedioEstudiante(): Double {
+        var totalNotas: Double = 0.0
+        for (calificacion in calificaciones) {
+            totalNotas += calificacion
+        }
+        return totalNotas / calificaciones.size
     }
 }
 
 class Curso {
     val nombreCurso: String = ""
-    var listadoEstudiantes: MutableList<Estudiante> = TODO()
+    var listadoEstudiantes: MutableList<Estudiante> = mutableListOf()
 
-    fun agregarEstudiante(){
-
+    fun agregarEstudiante(newStudent: Estudiante) {
+        listadoEstudiantes.add(newStudent)
     }
 
-    fun eliminarEstudiante(){
-
+    fun eliminarEstudiante(student: Estudiante) {
+        listadoEstudiantes.remove(student)
     }
 
-    fun mejorEstudiante(){
+    fun mejorEstudiante(): List<Estudiante> {
+        if (listadoEstudiantes.isEmpty()) return listOf()
+        var mejores: MutableList<Estudiante> = mutableListOf()
+        mejores.add(listadoEstudiantes.first())
+        var mejorProm: Double = mejores.first().promedioEstudiante()
 
+        for (estudiante in listadoEstudiantes) {
+            if (estudiante.id == listadoEstudiantes[0].id) continue
+            if (estudiante.promedioEstudiante() > mejorProm) {
+                mejores[0] = estudiante;
+                mejorProm = estudiante.promedioEstudiante()
+            } else if (estudiante.promedioEstudiante() == mejorProm) {
+                mejores.add(estudiante)
+            }
+        }
+        return mejores
     }
 }
 
